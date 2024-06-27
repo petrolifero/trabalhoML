06d4127ec03dd43c83a60c73eecb0f1ca04bdc67
João Pedro Abreu de Souza <jp_abreu@id.uff.br>
2024-06-27 00:00:40 -0300

    update

--- main.py
+++ main.py
@@ -11,6 +11,58 @@ def preprocess_data(df):
     df = pd.get_dummies(df, drop_first=True)
     return df
 
+def explain_decision_tree(clf):
+    n_nodes = clf.tree_.node_count
+    children_left = clf.tree_.children_left
+    children_right = clf.tree_.children_right
+    feature = clf.tree_.feature
+    threshold = clf.tree_.threshold
+    values = clf.tree_.value
+    node_depth = np.zeros(shape=n_nodes, dtype=np.int64)
+    is_leaves = np.zeros(shape=n_nodes, dtype=bool)
+    stack = [(0, 0)]  # start with the root node id (0) and its depth (0)
+    while len(stack) > 0:
+        # `pop` ensures each node is only visited once
+        node_id, depth = stack.pop()
+        node_depth[node_id] = depth
+        
+        # If the left and right child of a node is not the same we have a split
+        # node
+        is_split_node = children_left[node_id] != children_right[node_id]
+        # If a split node, append left and right children and depth to `stack`
+        # so we can loop through them
+        if is_split_node:
+            stack.append((children_left[node_id], depth + 1))
+            stack.append((children_right[node_id], depth + 1))
+        else:
+            is_leaves[node_id] = True
+            
+            print(
+                "The binary tree structure has {n} nodes and has "
+                "the following tree structure:\n".format(n=n_nodes)
+            )
+            for i in range(n_nodes):
+                if is_leaves[i]:
+                    print(
+                        "{space}node={node} is a leaf node with value={value}.".format(
+                            space=node_depth[i] * "\t", node=i, value=values[i]
+                        )
+                    )
+                else:
+                    print(
+                        "{space}node={node} is a split node with value={value}: "
+                        "go to node {left} if X[:, {feature}] <= {threshold} "
+                        "else to node {right}.".format(
+                            space=node_depth[i] * "\t",
+                            node=i,
+                            left=children_left[i],
+                            feature=feature[i],
+                            threshold=threshold[i],
+                            right=children_right[i],
+                            value=values[i],
+                        )
+        )
+
 def read_all_csv(csv_files):
     dataframes = []
     for file in csv_files:
@@ -45,10 +97,11 @@ for i, df in enumerate(dataframes):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
     # Criar e treinar o modelo
-    clf = DecisionTreeClassifier()
+    clf = DecisionTreeClassifier(max_depth=10)
     clf.fit(X_train, y_train)
     # Prever no conjunto de teste
     y_pred = clf.predict(X_test)
     # Mostrar relatório de classificação
     print(f"\nRelatório de classificação para o arquivo {csv_file}:")
     print(classification_report(y_test, y_pred))
+#    explain_decision_tree(clf)
